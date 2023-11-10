206e4df114d7960c63cc9483ce5f9781694208aa
omlan99 <95955202+omlan99@users.noreply.github.com>
2023-11-10 23:37:47 +0600

    modal+slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
-    $('.slider').slick({
-        autoplay:true,
-        arrows:false,
-        dots:true,
+  $('.slider').slick({
+    autoplay:true,
+    arrows:false,
+    dots:true,
 
-    });
-    $('.slide').slick({
-      autoplay:true,
-      slidesToShow: 4,
-      slidesToScroll: 2,
-      arrows: false,
-      dots:false,
-    })
   });
-      
+  $('.slide').slick({
+    autoplay:true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: false,
+    dots:false,
+  })
+  $('.shower').slick({
+    autoplay:true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    arrows: false,
+     dots:false,
+  })
+
+
+  // $('#search').on('click', function(){
+  //   $('#search_modal').addClass('show');
+  // })
+  $('#search').on('click', function(){
+    $('#search_modal').addClass('show');
+  })
+ 
+});
+
