e2d271142f9e2169f38e5ec3c68c365518aa2f8c
omlan99 <95955202+omlan99@users.noreply.github.com>
2023-11-14 11:29:12 +0600

    blog+features+slick.dots

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,19 +5,26 @@ $(document).ready(function(){
     dots:true,
 
   });
-  $('.slide').slick({
-    autoplay:true,
+  $('.slide_1').slick({
+    autoplay:false,
     slidesToShow: 4,
-    slidesToScroll: 2,
+    slidesToScroll: 1,
     arrows: false,
     dots:false,
   })
-  $('.shower').slick({
+  $('.slide_2').slick({
     autoplay:true,
     slidesToShow: 2,
     slidesToScroll: 2,
     arrows: false,
      dots:false,
+  })
+  $('.slide_3').slick({
+    autoplay: true,
+    slidesToShow:3,
+    slidesToScroll: 1,
+    arrows: false,
+    dots:true,
   })
 
 
